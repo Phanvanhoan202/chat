b2e68dd6a139e9c421459d2245d7c41d7342f1ff
Phanvanhoan202 <70952493+Phanvanhoan202@users.noreply.github.com>
2023-11-06 10:50:49 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import NoPage from './pages/NoPage';
+// import NoPage from './pages/NoPage';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
@@ -31,7 +31,7 @@ function App() {
                     />
                     <Route path="register" element={<Register />} />
                     <Route path="login" element={<Login />} />
-                    <Route path="*" element={<NoPage />} />
+                    // <Route path="*" element={<NoPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
