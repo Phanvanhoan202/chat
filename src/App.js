1da1560c693faa6671184a0217111de6875182da
Phanvanhoan202 <70952493+Phanvanhoan202@users.noreply.github.com>
2023-11-06 13:27:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             <Routes>
                 <Route path="/">
                     <Route
-                        index
+                        path="chat"
                         element={
                             <ProtectedRoute>
                                 <Home />
